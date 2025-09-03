c5f8676f99e0767eaa5d6af3ff7d7858735bf049
Artur Bomtempo Colen <arturbcolen@gmail.com>
2025-09-03 18:10:10 -0300

    feat: add CountdownTimer component and integrate CountdownElement for event countdown

--- src/pages/DevFest/index.tsx
+++ src/pages/DevFest/index.tsx
@@ -1,6 +1,12 @@
 import { Hero } from '../../components/Common/Hero';
 import devfestBg from '../../assets/pages/devfest/bg.png';
+import { CountdownTimer } from '../../components/pages/DevFest/CountdownTimer';
 
 export function DevFest() {
-    return <Hero title="DevFest 2025" location="Belo Horizonte" bgImage={devfestBg} />;
+    return (
+        <>
+            <Hero title="DevFest 2025" location="Belo Horizonte" bgImage={devfestBg} />
+            <CountdownTimer />
+        </>
+    );
 }
