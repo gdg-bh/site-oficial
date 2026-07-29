670a02bfcb0692c6a0ce2c8ac5c2d2aaa278a758
Guilherme Radicchi <guilhermeradicchi@hotmail.com>
2026-07-29 09:47:41 -0300

    feat: Add ContestWidget component for DevFest 2026 with contest image and form link

--- src/pages/DevFest2026/index.tsx
+++ src/pages/DevFest2026/index.tsx
@@ -1,6 +1,7 @@
 import devfestBg from '../../assets/pages/devfest2026/Img Header.png';
 import { HeroEvent } from '../../components/Common/HeroEvent';
 import AboutDevfest2026 from '../../components/pages/DevFest2026/AboutDevfest2026';
+import ContestWidget from '../../components/pages/DevFest2026/ContestWidget';
 import Speakers2026 from '../../components/pages/DevFest2026/Speakers2026';
 import Schedule2026 from '../../components/pages/DevFest2026/Schedule2026';
 import Sponsors2026 from '../../components/pages/DevFest2026/Sponsors2026';
@@ -9,6 +10,7 @@ export function DevFest2026() {
     return (
         <>
             <HeroEvent title="DevFest 2026" location="Belo Horizonte" bgImage={devfestBg} />
+            <ContestWidget />
             <AboutDevfest2026 />
             <Speakers2026 />
             {/* <Schedule2026 /> */}
